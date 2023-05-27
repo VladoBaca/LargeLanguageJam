aaab58c94baf0620d3210d729b9e879a61f0bbb2
Vladimír Bača <vladimir.baca@thermofisher.com>
2023-05-27 15:16:09 +0200

    Perturbator improvements

--- perturbator.py
+++ perturbator.py
@@ -2,18 +2,28 @@ import random
 import string
 from datetime import datetime
 
+random.seed(1024*datetime.now().timestamp())
 
-def perturb(text, prob=0.05):
-    random.seed(datetime.now().timestamp())
-    chars = list(text)
-    for idx, c in enumerate(chars):
-        if random.random() < prob:
-            chars[idx] = get_random_char()
+
+def perturb(text, prob=0.05, letters=string.ascii_letters):
+    chars = []
+    for idx, c in enumerate(text):
+        if (not c.isspace()) and random.random() < prob:
+            op = random.choice(['a', 'r', 's'])
+            match op:
+                case 'a':
+                    chars.append(get_random_char(letters))
+                    chars.append(c)
+                case 's':
+                    chars.append(get_random_char(letters))
+        else:
+            chars.append(c)
     return ''.join(chars)
 
 
-def get_random_char():
-    return random.choice(string.ascii_letters)
+def get_random_char(letters=string.ascii_letters):
+    return random.choice(letters)
 
 
-print(perturb("Hello there! Is there a typo in this sentence? Are you sure?"))
+# print(perturb("Hello there! Is there a typo in this sentence? Are you sure?", 0.1))
+# print(perturb("Hello there! Is there a typo in this sentence? Are you sure?", 0.1, "xyz"))
