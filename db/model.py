ce0dc0b68581eca89bce9bd666993704c561de6b
Jakub Vonšovský <jakub.vonsovsky@gmail.com>
2023-05-27 16:42:47 +0200

    Vectorization support

--- db/model.py
+++ db/model.py
@@ -15,7 +15,7 @@ class DBModel:
     def select_sentences(self, language: str = "en", topic: Optional[int] = None) -> List[str]:
         try:
             with self.connection as cursor:
-                query = "SELECT text, vector FROM sentences WHERE language = ?"
+                query = "SELECT id, text, vector FROM sentences WHERE language = ?"
                 vals = [language]
                 if topic is not None:
                     query += " AND topic = ?"
@@ -28,7 +28,7 @@ class DBModel:
             logging.warning(f"Could not select records: {ex}")
 
     def insert_sentence(self, text: str, language: str = "en", topic: Optional[int] = None):
-        query = "INSERT INTO sentences (text, language, topic_id) VALUES (?, ?, ?)"
+        query = f"INSERT INTO sentences (text, language, topic_id) VALUES (?, ?, ?)"
 
         try:
             with self.connection as cursor:
@@ -36,13 +36,28 @@ class DBModel:
         except sqlite3.DatabaseError as ex:
             logger.error(ex)
 
+    def insert_perturbance(self, sentence_id: int, text: str):
+        query = f"INSERT INTO perturbances (sentence_id, text) VALUES (?, ?)"
+
+        try:
+            with self.connection as cursor:
+                cursor.execute(query, (sentence_id, text))
+        except sqlite3.DatabaseError as ex:
+            logger.error(ex)
+
     def update_sentence(self, s_id, **kwargs):
-        query = "UPDATE playlist "
+        self.update_table("sentences", s_id, kwargs=kwargs)
+
+    def update_perturbance(self, s_id, **kwargs):
+        self.update_table("perturbances", s_id, kwargs=kwargs)
+
+    def update_table(self, table_name, s_id, **kwargs):
+        query = f"UPDATE {table_name} SET"
         vals = []
         for arg, val in kwargs.items():
-            query += f", {arg}=?"
+            query += f" {arg}=?,"
             vals.append(val)
-        query += " WHERE id=?"
+        query = query[:len(query) - 1] + " WHERE id=?"
         vals.append(s_id)
 
         try:
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from init import db_model
+import json
+import sqlite3
+
 from api.openai import get_vector
+from init import db_model
+
+
+def update_vector(sentence: sqlite3.Row):
+    vector = json.dumps(get_vector(sentence["text"]))
+    db_model.update_sentence(sentence["id"], vector=vector)
+
+
+def pipeline():
+    sentences = db_model.select_sentences("cz")
+    for sentence in sentences:
+        if sentence["vector"] is None:
+            update_vector(sentence)
 
 
 if __name__ == "__main__":
-    db_model.insert_sentence("Ahoj svete", "cz")
-
-    sentences = db_model.select_sentences("cz")
-    for sentence in sentences:
-        print(sentence["text"])
-        if sentence["vector"] is None:
-            # TODO vector
-            db_model.update_sequence(sentence["id"], vector)
+    pipeline()
